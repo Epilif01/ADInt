e92fd6afa324be4077e654f102c5b3a9a8d85221
Aimnos <roikegami@hotmail.com>
2023-10-04 15:51:10 +0100

    Check In App

--- Projeto/check_in.py
+++ Projeto/check_in.py
@@ -1,4 +1,11 @@
-from flask import Flask, render_template, request, send_from_directory, redirect, jsonify
+from flask import (
+    Flask,
+    render_template,
+    request,
+    send_from_directory,
+    redirect,
+    jsonify,
+)
 import os
 import datetime
 import checkin_db as db
@@ -6,33 +13,54 @@ from os.path import exists
 
 app = Flask(__name__)
 
+
 @app.route("/")
 @app.route("/index")
 def index():
     return render_template("check_in_app.html")
 
-@app.route("/check_in", methods=['GET', 'POST'])
+
+@app.route("/check_in", methods=["GET", "POST"])
 def check_in():
-    if request.method == 'GET':
+    if request.method == "GET":
         return render_template("check_in.html")
     else:
         print(request.form)
-        place_id = request.form['place_id']
-        user_id = request.form['user_id']
+        place_id = request.form["place_id"]
+        user_id = request.form["user_id"]
+        if db.getCheckin(user_id) is not None:
+            return redirect("/check_in_failed/" + place_id)
+
         timeofcheckin = datetime.datetime.now()
         timeofcheckout = None
         db.new_checkin(place_id, user_id, timeofcheckin, timeofcheckout)
         return redirect("/")
-    
-@app.route("/check_out", methods=['GET', 'POST'])
+
+
+@app.route("/check_out", methods=["GET", "POST"])
 def check_out():
-    if request.method == 'GET':
+    if request.method == "GET":
         return render_template("check_out.html")
     else:
         print(request.form)
-        place_id = request.form['place_id']
-        user_id = request.form['user_id']
+        user_id = request.form["user_id"]
         timeofcheckout = datetime.datetime.now()
-        checkout_state = db.checkout(place_id, user_id, timeofcheckout)
+        checkout_state = db.checkout(user_id, timeofcheckout)
         print(checkout_state)
+        if checkout_state == "No checkin found":
+            return redirect("/check_out_failed")
         return redirect("/")
+
+
+@app.route("/check_out_failed")
+def check_out_failed():
+    return render_template("check_out_failed.html")
+
+
+@app.route("/check_in_failed/<path:place_id>")
+def check_in_failed(place_id):
+    return render_template("check_in_failed.html", place_id=place_id)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8003, debug=True)
--- Projeto/checkin_db.py
+++ Projeto/checkin_db.py
@@ -9,20 +9,23 @@ from sqlalchemy.orm import sessionmaker
 from os import path
 
 
-#SLQ access layer initialization
+# SLQ access layer initialization
 DATABASE_FILE = "checkin.sqlite"
 db_exists = False
 if path.exists(DATABASE_FILE):
     db_exists = True
     print("\t database already exists")
 
-engine = create_engine('sqlite:///%s'%(DATABASE_FILE), echo=False) #echo = True shows all SQL calls
+engine = create_engine(
+    "sqlite:///%s" % (DATABASE_FILE), echo=False
+)  # echo = True shows all SQL calls
 
 Base = declarative_base()
 
-#Declaration of data
+
+# Declaration of data
 class Book(Base):
-    __tablename__ = 'book'
+    __tablename__ = "book"
     id = Column(Integer, primary_key=True)
     place_id = Column(String, nullable=False)
     user_id = Column(String, nullable=False)
@@ -30,24 +33,46 @@ class Book(Base):
     timeofcheckout = Column(Date, nullable=True)
 
     def __repr__(self):
-        return "<Book(place_id='%s', user_id='%s', timeofcheckin='%s', timeofcheckout='%s')>" % (
-                                self.place_id, self.user_id, self.timeofcheckin, self.timeofcheckout)
+        return (
+            "<Book(place_id='%s', user_id='%s', timeofcheckin='%s', timeofcheckout='%s')>"
+            % (self.place_id, self.user_id, self.timeofcheckin, self.timeofcheckout)
+        )
 
-Base.metadata.create_all(engine) #Create tables for the data models
+
+Base.metadata.create_all(engine)  # Create tables for the data models
 
 Session = sessionmaker(bind=engine)
 session = Session()
 
+
 def new_checkin(place_id, user_id, timeofcheckin, timeofcheckout):
-    book = Book(place_id=place_id, user_id=user_id, timeofcheckin=timeofcheckin, timeofcheckout=timeofcheckout)
+    book = Book(
+        place_id=place_id,
+        user_id=user_id,
+        timeofcheckin=timeofcheckin,
+        timeofcheckout=timeofcheckout,
+    )
     session.add(book)
     session.commit()
 
+
 def checkout(place_id, user_id, timeofcheckout):
-    book = session.query(Book).filter_by(place_id=place_id, user_id=user_id, timeofcheckout=None).first()
+    book = (
+        session.query(Book)
+        .filter_by(place_id=place_id, user_id=user_id, timeofcheckout=None)
+        .first()
+    )
     if book is None:
         return "No checkin found"
     else:
         book.timeofcheckout = timeofcheckout
         session.commit()
         return "Check out successful"
+
+
+def getCheckin(user_id):
+    book = session.query(Book).filter_by(user_id=user_id, timeofcheckout=None).first()
+    if book is None:
+        return None
+    else:
+        return book.place_id
