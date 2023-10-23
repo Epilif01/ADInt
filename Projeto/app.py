ea028621078609ee56555343ac22271cd6a7551a
Epilif01 <epilif.seravat@gmail.com>
2023-10-23 19:00:25 +0100

    QrCode with button

--- Projeto/app.py
+++ Projeto/app.py
@@ -16,8 +16,6 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'top secret!'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['OAUTH2_PROVIDERS'] = {
-    # Google OAuth 2.0 documentation:
-    # https://developers.google.com/identity/protocols/oauth2/web-server#httprest
     'fenix': {
         'client_id': os.environ.get('FENIX_CLIENT_ID'),
         'client_secret': os.environ.get('FENIX_CLIENT_SECRET'),
@@ -28,33 +26,8 @@ app.config['OAUTH2_PROVIDERS'] = {
             'email': lambda json: json['email'],
         },
         'scopes': [],
-       # 'scopes': ['https://www.googleapis.com/auth/userinfo.email'],
     },
-    'google': {
-        'client_id': os.environ.get('GOOGLE_CLIENT_ID'),
-        'client_secret': os.environ.get('GOOGLE_CLIENT_SECRET'),
-        'authorize_url': 'https://accounts.google.com/o/oauth2/auth',
-        'token_url': 'https://accounts.google.com/o/oauth2/token',
-        'userinfo': {
-            'url': 'https://www.googleapis.com/oauth2/v3/userinfo',
-            'email': lambda json: json['email'],
-        },
-        'scopes': ['https://www.googleapis.com/auth/userinfo.email'],
-    },
-
-    # GitHub OAuth 2.0 documentation:
-    # https://docs.github.com/en/apps/oauth-apps/building-oauth-apps/authorizing-oauth-apps
-    'github': {
-        'client_id': os.environ.get('GITHUB_CLIENT_ID'),
-        'client_secret': os.environ.get('GITHUB_CLIENT_SECRET'),
-        'authorize_url': 'https://github.com/login/oauth/authorize',
-        'token_url': 'https://github.com/login/oauth/access_token',
-        'userinfo': {
-            'url': 'https://api.github.com/user/emails',
-            'email': lambda json: json[0]['email'],
-        },
-        'scopes': ['user:email'],
-    },
+    
 }
 
 db = SQLAlchemy(app)
@@ -77,7 +50,7 @@ def load_user(id):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('app_index.html')
 
 @app.route('/other')
 def other_route():
